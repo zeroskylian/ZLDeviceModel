18c3b82ef5731c7ace7e96d8718113a5ffe72008
zeroskylian <zeroskylian@gmail.com>
2024-03-20 15:21:38 +0800

    add iPad

--- Core/ZLDeviceModel.swift
+++ Core/ZLDeviceModel.swift
@@ -69,16 +69,22 @@ struct ZLDeviceModel {
         case iPad5                 = "iPad 5"
         case iPad6                 = "iPad 6"
         case iPad7                 = "iPad 7"
+        case iPad8                 = "iPad 8"
+        case iPad9                 = "iPad 9"
+        case iPad10                 = "iPad 10"
         
         case iPadMini              = "iPad Mini"
         case iPadMini2             = "iPad Mini 2"
         case iPadMini3             = "iPad Mini 3"
         case iPadMini4             = "iPad Mini 4"
         case iPadMini5             = "iPad Mini 5"
+        case iPadMini6             = "iPad Mini 6"
         
         case iPadAir               = "iPad Air"
         case iPadAir2              = "iPad Air 2"
         case iPadAir3              = "iPad Air 3"
+        case iPadAir4              = "iPad Air 4"
+        case iPadAir5              = "iPad Air 5"
         
         case iPadPro12Dot9Inch     = "iPad Pro 12.9 Inch"
         case iPadPro9Dot7Inch      = "iPad Pro 9.7 Inch"
@@ -89,6 +95,8 @@ struct ZLDeviceModel {
         case iPadPro12Dot9Inch3Gen = "iPad Pro 12.9 Inch 3"
         case iPadPro11Inch2        = "iPad Pro 11 Inch 2"
         case iPadPro12Dot9Inch4Gen = "iPad Pro 12.9 Inch 4"
+        case iPadPro11Inch3        = "iPad Pro 11 Inch 3"
+        case iPadPro12Dot9Inch5Gen = "iPad Pro 12.9 Inch 5"
         
         case iPodTouch1Gen         = "iPod Touch 1"
         case iPodTouch2Gen         = "iPod Touch 2"
@@ -197,22 +205,18 @@ struct ZLDeviceModel {
             "iPad2,2"  : .iPad2,
             "iPad2,3"  : .iPad2,
             "iPad2,4"  : .iPad2,
-            
             "iPad2,5"  : .iPadMini,
             "iPad2,6"  : .iPadMini,
             "iPad2,7"  : .iPadMini,
-            
             "iPad3,1"  : .iPad3,
             "iPad3,2"  : .iPad3,
             "iPad3,3"  : .iPad3,
             "iPad3,4"  : .iPad4,
             "iPad3,5"  : .iPad4,
             "iPad3,6"  : .iPad4,
-            
             "iPad4,1"  : .iPadAir,
             "iPad4,2"  : .iPadAir,
             "iPad4,3"  : .iPadAir,
-            
             "iPad4,4"  : .iPadMini2,
             "iPad4,5"  : .iPadMini2,
             "iPad4,6"  : .iPadMini2,
@@ -221,7 +225,6 @@ struct ZLDeviceModel {
             "iPad4,9"  : .iPadMini3,
             "iPad5,1"  : .iPadMini4,
             "iPad5,2"  : .iPadMini4,
-            
             "iPad5,3"  : .iPadAir2,
             "iPad5,4"  : .iPadAir2,
             "iPad6,3"  : .iPadPro9Dot7Inch,
@@ -238,28 +241,43 @@ struct ZLDeviceModel {
             "iPad7,6"  : .iPad6,
             "iPad7,11" : .iPad7,
             "iPad7,12" : .iPad7,
-            
             "iPad8,1"  : .iPadPro11Inch,
             "iPad8,2"  : .iPadPro11Inch,
             "iPad8,3"  : .iPadPro11Inch,
             "iPad8,4"  : .iPadPro11Inch,
-            
             "iPad8,5"  : .iPadPro12Dot9Inch3Gen,
             "iPad8,6"  : .iPadPro12Dot9Inch3Gen,
             "iPad8,7"  : .iPadPro12Dot9Inch3Gen,
             "iPad8,8"  : .iPadPro12Dot9Inch3Gen,
-            
             "iPad8,9"  : .iPadPro11Inch2,
             "iPad8,10" : .iPadPro11Inch2,
-            
             "iPad8,11" : .iPadPro12Dot9Inch4Gen,
             "iPad8,12" : .iPadPro12Dot9Inch4Gen,
-            
-            
             "iPad11,1" : .iPadMini5,
             "iPad11,2" : .iPadMini5,
             "iPad11,3" : .iPadAir3,
             "iPad11,4" : .iPadAir3,
+            "iPad11,6" : .iPad8,
+            "iPad11,7" : .iPad8,
+            "iPad12,1" : .iPad9,
+            "iPad12,2" : .iPad9,
+            "iPad13,1" : .iPadAir4,
+            "iPad13,2" : .iPadAir4,
+            "iPad13,4" : .iPadPro11Inch3,
+            "iPad13,5" : .iPadPro11Inch3,
+            "iPad13,6" : .iPadPro11Inch3,
+            "iPad13,7" : .iPadPro11Inch3,
+            "iPad13,8" : .iPadPro12Dot9Inch5Gen,
+            "iPad13,9" : .iPadPro12Dot9Inch5Gen,
+            "iPad13,10" : .iPadPro12Dot9Inch5Gen,
+            "iPad13,11" : .iPadPro12Dot9Inch5Gen,
+            "iPad13,16" : .iPadAir5,
+            "iPad13,17" : .iPadAir5,
+            "iPad13,18" : .iPad10,
+            "iPad13,19" : .iPad10,
+            "iPad14,1" : .iPadMini6,
+            "iPad14,2" : .iPadMini6,
+            
             
             //iPods
             "iPod1,1" : .iPodTouch1Gen,
